e84cc4cc62389c57d5b2620eae26215881c128d6
Martin Lenart <martin_lenart@icloud.com>
2021-10-13 12:24:01 +0200

    Add project files.

---
+++ GroupAssignment3/StudentList.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace GroupAssignment3
+{
+/*
+    class StudentList: IStudentList
+    {
+        const int MaxNrOfStudents = 50;
+        string[] students;
+
+        private int _NrOfStudents = 0;
+        public int NrOfStudents => _NrOfStudents;
+
+        public override string ToString()
+        {
+            string sRet = "";
+            for (int i = 0; i < _NrOfStudents; i++)
+            {
+                sRet += $"{students[i],-15}";
+                if ((i + 1) % 5 == 0)
+                    sRet += "\n";
+            }
+            return sRet;
+        }
+
+        public void CreateOOP1dotNet5()
+        {
+            students = new string[MaxNrOfStudents];
+            students[0] = "Sahar";
+            students[1] = "Jennifer";
+            students[2] = "Shohruh";
+            students[3] = "Jonathan";
+            students[4] = "Leo";
+            students[5] = "Jenny";
+            students[6] = "Mohamed";
+            students[7] = "Ferri";
+            students[8] = "Alexandra F";
+            students[9] = "Vidar";
+            students[10] = "Kamran";
+            students[11] = "Pontus";
+            students[12] = "Kaveh";
+            students[13] = "Maria";
+            students[14] = "Adam";
+            students[15] = "Sophie";
+            students[16] = "Louise";
+            students[17] = "Fredric";
+            students[18] = "Carl-Henrik";
+            students[19] = "Frans";
+            students[20] = "Sam";
+            students[21] = "Alexandra S";
+            students[22] = "Alperen";
+            students[23] = "Josefine";
+            students[24] = "Ghasem";
+            students[25] = "Hanna";
+            students[26] = "Finan";
+            students[27] = "Niklas";
+            students[28] = "Hector";
+            students[29] = "Fredrik";
+            students[30] = "Adrian";
+            students[31] = "Teodor";
+
+            _NrOfStudents = 30;
+        }
+    }
+*/
+}
